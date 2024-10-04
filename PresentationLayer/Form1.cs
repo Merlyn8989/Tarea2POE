510f8d0f4de2e43e2a31ab83c1bf0eb91e9f3f31
Merlyn8989 <merlynrajo@gmail.com>
2024-10-04 11:47:07 -0600

    borrando

--- PresentationLayer/Form1.cs
+++ PresentationLayer/Form1.cs
@@ -6,5 +6,10 @@ namespace PresentationLayer
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
